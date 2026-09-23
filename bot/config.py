25e3ba6fa659889072e62e36335f0b3ec47d926a
Alexaditya099999 <deepakkr9104@zohomail.in>
2026-09-23 11:50:46 +0530

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -17,13 +17,13 @@ def is_enabled(value, default):
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "tg_bot")
-    DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+    API_ID = int(os.environ.get("22544661"))
+    API_HASH = os.environ.get("5dbcbc7c7388e8442f5da278b5914610")
+    BOT_TOKEN = os.environ.get("7611901718:AAEusvOY57OodWl_WgwcEVm-RiY0MysGMFI")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "drmtest")
+    DATABASE_URL = os.environ.get("http://t.me/Drmtest1804_bot", None)
+    OWNER_ID = int(os.environ.get("6960427846")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ("-1002463068122")
     WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "False"), False)
     THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "").split()))
 
@@ -36,7 +36,7 @@ class Script(object):
     START_MESSAGE = (
         " {mention}\n\nSend any link or set of links in a txt file to download them."
     )
-    DEV_MESSAGE = """👋 Hey there, I'm Caption Alex ❤️– your go-to Telegram bot developer!
+    DEV_MESSAGE = """👋 Hey there, I'm Alex Aditya ❤️– your go-to Telegram bot developer!
 
 🤖 Love having bots that do the heavy lifting for you? That's my jam! I'm all about crafting super cool and custom Telegram bots that make your life a breeze.
 
@@ -47,7 +47,7 @@ class Script(object):
 - **Chill Vibes:** I keep your data super safe, so you can relax and enjoy the bot party.
 - **Always Improving:** Telegram evolves, and my bots grow with it. I'm here to keep things fresh and fab.
 
-Ready for your own bot buddy? Ping me on [Telegram](https://telegram.me/Alex_Aditya9) or check out me on [GitHub](https://github.com/The_real_xTaR). Wanna hire me? Find me on [Fiverr](https://www.fiverr.com/The_real_xTaR)!
+Ready for your own bot buddy? Ping me on [Telegram](https://telegram.me/Reason_Someone) or check out me on [GitHub](https://github.com/The_real_xTaR). Wanna hire me? Find me on [Fiverr](https://www.fiverr.com/The_real_xTaR)!
 
 Let's bot up and have some fun! 🤘"""
     HELP_MESSAGE = os.environ.get("HELP_MESSAGE", "Help message")
@@ -75,7 +75,7 @@ Let's bot up and have some fun! 🤘"""
 
 Original Index: {orginal_start_index}/{orginal_end_index}
 
-[Caption Alex ❤️](https://t.me/Alex_Aditya9)"""
+[Alex Aditya ❤️](https://t.me/Reason_Someone)"""
 
     DEFAULT_CAPTION = """[📁] File_ID : {file_index}
 
@@ -85,7 +85,7 @@ Original Index: {orginal_start_index}/{orginal_end_index}
 
 📚 Bᴀᴛᴄʜ Nᴀᴍᴇ : {batch_name}
 
-Dᴏᴡɴʟᴏᴀᴅᴇᴅ Bʏ : [Caption Alex ❤️](https://t.me/Alex_Aditya9)"""
+Dᴏᴡɴʟᴏᴀᴅᴇᴅ Bʏ : [Alex Aditya ❤️](https://t.me/ReaSon_SomeOne_Bot)"""
 
 
     CAPTION_CB = """**Set Caption
@@ -111,3 +111,4 @@ Dᴏᴡɴʟᴏᴀᴅᴇᴅ Bʏ : [Caption Alex ❤️](https://t.me/Alex_Aditya9
 `{default_caption}`
 
 ➢ **Status:** {status}"""
+
