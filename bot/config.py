da0271a36c559e2b61df7431892c574c6c7b0296
Alexaditya099999 <deepakkr9104@zohomail.in>
2026-09-23 10:45:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -17,12 +17,12 @@ def is_enabled(value, default):
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("20346550"))
+    API_HASH = os.environ.get("bc79c3bea7a626887bdc0871eecf0327")
+    BOT_TOKEN = os.environ.get("8979171607:AAHx9a8p9YtvOjyjyKcT7q87ASVUhN7pj1I")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "tg_bot")
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    OWNER_ID = int(os.environ.get("8460497291"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
     WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "False"), False)
     THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "").split()))
